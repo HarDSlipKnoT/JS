64298f4e1d920e2f4ffed2c90508322732cdbbf9
Slipknot <dreadisback666@mail.ru>
2023-06-22 01:37:10 +0500

    Js added

---
+++ script.js
@@ -0,0 +1,26 @@
+var userName = prompt("Введите своё имя");
+console.log(userName);
+
+var userAge = +prompt("Введите свой возраст");
+console.log(userAge);
+
+
+var pr1 = +prompt("88+22=?")
+console.log('Пример 1: 88 + 22 = ' + (88 + 22) + " (Ваш ответ" + pr1 + " )");
+
+var pr2 = +prompt("15-10=?")
+console.log('Пример 2: 15 - 10 = ' + (15 - 10) + " (Ваш ответ" + pr2 + " )");
+
+var pr3 = +prompt("7*7=?")
+console.log('Пример 3: 7 * 7 = ' + 7 * 7 + " (Ваш ответ" + pr3 + " )");
+
+var pr4 = +prompt("14/2=?")
+console.log('Пример 4: 14 / 2 = ' + 14 / 2 + " (Ваш ответ" + pr4 + " )");
+
+var pr5 = +prompt("30%250=?")
+console.log('Пример 5: 30 % 250 = ' + 30 % 250 + " (Ваш ответ" + pr5 + " )");
+
+var userMessage = alert("Ваши ответы в консоли разработчика")
+console.log(userMessage);
+alert(userMessage)
+
